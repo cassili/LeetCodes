c6588b2f6e5e0f2ecea7904813340657664b217a
Rongji Han <hanrongji@bonc.com.cn>
2021-10-02 22:20:02 +0800

    Solution

---
+++ src/com/LengthOfLastWord58/Solution.java
@@ -0,0 +1,14 @@
+package com.LengthOfLastWord58;
+
+/**
+ * @Description:
+ * @Author: hhh
+ * @Version:
+ * @Date: 2021/10/2 22:19
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
