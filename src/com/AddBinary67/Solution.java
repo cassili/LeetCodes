fb83e000c4aa69a52189b43d5fdda05431ee83a9
han_rongji@163.com <han_rongji@163.com>
2021-10-05 23:52:21 +0800

    二进制字符串相加

--- src/com/AddBinary67/Solution.java
+++ src/com/AddBinary67/Solution.java
@@ -74,6 +74,38 @@ public class Solution {
         return sb.toString();
     }
 
+    /**
+     * 优秀解法
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public String addBinary2(String a, String b) {
+        char[] aArray = a.toCharArray(), bArray = b.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        int i = aArray.length - 1, j = bArray.length - 1, carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = (i >= 0 ? aArray[i] - '0' : 0) + (j >= 0 ? bArray[j] - '0' : 0) + carry;
+
+            sb.append(sum % 2);
+            carry = sum / 2;
+
+            if (i >= 0) i--;
+            if (j >= 0) j--;
+        }
+
+        if (carry != 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+
+    }
+
+
     public static void main(String[] args) {
         String a = "1011", b = "1110";
         System.out.println(addBinary(a, b));
