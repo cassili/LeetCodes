47ebe9e94288f1c24b5c113daf9dbf2e30cc5e86
han_rongji@163.com <han_rongji@163.com>
2021-10-05 23:43:36 +0800

    二进制字符串相加

--- src/com/AddBinary67/Solution.java
+++ src/com/AddBinary67/Solution.java
@@ -12,12 +12,12 @@ public class Solution {
     public static String addBinary(String a, String b) {
 
         int c = 0;
-        StringBuffer sb = new StringBuffer("");
+        StringBuilder sb = new StringBuilder("");
 
         // 能相加的位--相加
         for (int i = a.length() - 1, j = b.length() - 1; i >= 0 && j >= 0; i--, j--) {
-            int a_num = a.charAt(i) -'0';
-            int b_num = b.charAt(j) -'0';
+            int a_num = a.charAt(i) - '0';
+            int b_num = b.charAt(j) - '0';
 
             if (a_num + b_num + c == 0) {
                 c = 0;
@@ -35,65 +35,47 @@ public class Solution {
 
         }
 
-        System.out.println(c);
-
         // 高位不能相加的保留下来，拼到前面
-        int a_len = a.length();
-        int b_len = b.length();
-        int j = 0;
-        if (a_len - b_len > 0) {  // a 长
-            j = a_len - b_len - 1;
-            // 能相加的位--相加
-            for (; j >= 0; j--) {
-                int a_num = a.charAt(j) - '0';
-                if (a_num + c == 0) {
-                    c = 0;
-                    sb.append('0');
-                } else if (a_num + c == 1) {
-                    c = 0;
-                    sb.append('1');
-                } else {
-                    c = 1;
-                    sb.append('0');
-                }
-
-            }
-            if (c == 1) {
-                sb.append('1');
-            }
-
-        } else if (a_len == b_len) {  // a 和 b 一样长
+        String lowBits = null;
+        if (a.length() - b.length() > 0) {  // a 长
+            lowBits = LowBits(a, b, c, sb);
+        } else if (a.length() == b.length()) {  // a 和 b 一样长
             if (c == 1) {
                 sb.append('1');
             }
         } else {  // b 长
-            j = b_len - a_len - 1;
-            // 能相加的位--相加
-            for (; j >= 0; j--) {
-                int b_num = b.charAt(j) - '0';
-                if (b_num + c == 0) {
-                    c = 0;
-                    sb.append('0');
-                } else if (b_num + c == 1) {
-                    c = 0;
-                    sb.append('1');
-                } else {
-                    c = 1;
-                    sb.append('0');
-                }
-
-            }
-            if (c == 1) {
-                sb.append('1');
-            }
+            lowBits = LowBits(b, a, c, sb);
         }
 
         return sb.reverse().toString();
 
     }
 
+    public static String LowBits(String a, String b, int c, StringBuilder sb) {
+        int j = a.length() - b.length() - 1;
+        // 能相加的位--相加
+        for (; j >= 0; j--) {
+            int a_num = a.charAt(j) - '0';
+            if (a_num + c == 0) {
+                c = 0;
+                sb.append('0');
+            } else if (a_num + c == 1) {
+                c = 0;
+                sb.append('1');
+            } else {
+                c = 1;
+                sb.append('0');
+            }
+
+        }
+        if (c == 1) {
+            sb.append('1');
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        String a = "1011", b = "111";
+        String a = "1011", b = "1110";
         System.out.println(addBinary(a, b));
     }
 }
