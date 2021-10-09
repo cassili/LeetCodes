d2c3c9056fbfc5c17348db079d83d4bb757609b4
han_rongji@163.com <han_rongji@163.com>
2021-10-09 19:35:31 +0800

    s是t的字串，t中去掉几个字符形成的s

--- src/com/IsSubsequence392/Solution.java
+++ src/com/IsSubsequence392/Solution.java
@@ -7,9 +7,29 @@ package com.IsSubsequence392;
  * @Date: 2021/10/8 11:46
  */
 
+/**
+ * Runtime: 1 ms, faster than 83.11% of Java online submissions for Is Subsequence.
+ * Memory Usage: 37.4 MB, less than 48.31% of Java online submissions for Is Subsequence.
+ */
 public class Solution {
     public static boolean isSubsequence(String s, String t) {
 
+        for (Character c : s.toCharArray()) {
+            boolean flag = false;
+            for (int i = 0; i < t.length(); i++) {
+                if (t.charAt(i) == c) {
+                    t = t.substring(i + 1, t.length());
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag == false) {
+                return false;
+            }
+
+        }
+
+        return true;
 
     }
 
