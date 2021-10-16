b96cd93834f745f9c69d79388375e67bf5f75a69
cassili <han_rongji@163.com>
2021-10-16 15:26:46 +0800

    比较两棵树是否是一棵对称树

--- src/com/Tree/SymmetricTree101/Solution.java
+++ src/com/Tree/SymmetricTree101/Solution.java
@@ -27,11 +27,20 @@ class TreeNode {
 }
 
 public class Solution {
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Symmetric Tree.
+     * Memory Usage: 36.8 MB, less than 93.46% of Java online submissions for Symmetric Tree.
+     *
+     * @param r1
+     * @param r2
+     * @return
+     */
     public boolean compare(TreeNode r1, TreeNode r2) {
-        if (r1 == null && r2 == null)  return true;
-        else if (r1 == null || r2 == null || r1.val != r2.val)   return false;
+        if (r1 == null && r2 == null) return true;
+        else if (r1 == null || r2 == null || r1.val != r2.val) return false;
         return compare(r1.left, r2.right) && compare(r1.right, r2.left);
     }
+
     public boolean isSymmetric(TreeNode root) {
         return compare(root.left, root.right);
     }
